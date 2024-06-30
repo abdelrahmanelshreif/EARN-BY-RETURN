7ce20c6c7d87941e027f42fcd0525065e9699d4a
Abdelrahman Elshreif <link93955@gmail.com>
2024-06-30 03:29:23 +0300

    last edit

--- controllers/buissnessController.js
+++ controllers/buissnessController.js
@@ -1,25 +1,38 @@
 const gift = require('../model/giftModel');
+const user = require('../model/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
 
 exports.getProfit = catchAsync(async(req,res,next)=>{
   const gifts = await gift.find();
+  const users = await user.find();
   let totalGiftMoney = 0;
+  let totalGiftCoins = 0;
   let totalCans = 0;
   let totalBottles = 0;
+  let usersCount= 0;
 
   gifts.forEach(gift => {
     totalGiftMoney += gift.giftMoney;
+    totalGiftCoins += gift.giftCoins;
     totalCans += gift.noOfCans;
     totalBottles += gift.noOfBottles;
   });
 
+  users.forEach(user => {
+
+    usersCount +=1;
+  });
+
+
   let tot_profit = totalGiftMoney*1.5;
 
   res.status(200).json({
-    profit: tot_profit,
+    tot_users: usersCount,
     totalCans: totalCans,
+    totalCoins: totalGiftCoins,
+    profit: tot_profit,
     totalBottles: totalBottles,
     
   });
